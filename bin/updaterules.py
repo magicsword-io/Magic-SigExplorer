23950f4b36c2ae60c63b3aafa8142a8f2b06a847
The Haag <5632822+MHaggis@users.noreply.github.com>
2023-12-21 10:44:02 -0700

    Update updaterules.py

--- bin/updaterules.py
+++ bin/updaterules.py
@@ -35,11 +35,13 @@ def main():
         {
             'url': 'https://www.snort.org/downloads/community/community-rules.tar.gz',
             'file_path': 'rule_files/community-rules.tar.gz',
-            'is_tar': True
+            'is_tar': True,
+            'extracted_file_path': 'rule_files/community-rules/community.rules',
+            'final_file_path': 'rule_files/snort3-community.rules'
         },
         {
             'url': 'https://rules.emergingthreats.net/open/suricata-5.0/emerging-all.rules',
-            'file_path': 'rule_files/suricata-emerging-all.rules'  # New rule
+            'file_path': 'rule_files/suricata-emerging-all.rules'
         }
     ]
 
@@ -55,18 +57,36 @@ def main():
                     print(f"File {rule['file_path']} downloaded and updated.")
                     print(f"Old hash: {old_hash}")
                     print(f"New hash: {new_hash}")
+
                     if rule.get('is_tar', False):
                         try:
                             with tarfile.open(rule['file_path'], 'r:gz') as tar:
                                 tar.extractall(path='rule_files/')
                             print(f"Extracted {rule['file_path']}")
+
+                            extracted_file_path = rule.get('extracted_file_path')
+                            final_file_path = rule.get('final_file_path')
+                            if extracted_file_path and final_file_path:
+                                extracted_file_exists = os.path.exists(extracted_file_path)
+                                extracted_file_hash = get_file_hash(extracted_file_path) if extracted_file_exists else None
+                                final_file_exists = os.path.exists(final_file_path)
+                                final_file_hash = get_file_hash(final_file_path) if final_file_exists else None
+
+                                if not final_file_exists or final_file_hash != extracted_file_hash:
+                                    shutil.move(extracted_file_path, final_file_path)
+                                    print(f"Renamed {extracted_file_path} to {final_file_path}.")
+                                else:
+                                    print(f"No update needed for {final_file_path}.")
+
                             extracted_dir = 'rule_files/community-rules'
                             if os.path.isdir(extracted_dir):
                                 shutil.rmtree(extracted_dir)
                             os.remove(rule['file_path'])
                             print(f"Cleaned up extracted files and {rule['file_path']}")
+
                         except tarfile.ReadError as e:
                             print(f"Error extracting tar file {rule['file_path']}: {e}")
+
                 else:
                     print(f"No update needed for {rule['file_path']}.")
             else:
@@ -74,5 +94,6 @@ def main():
         else:
             print(f"Error occurred with file {rule['file_path']}. Exiting.")
             break
+
 if __name__ == '__main__':
     main()
